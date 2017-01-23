e7c111bb7e85da9b0628b49e4b668151d9fa4ed2
Mikhail Itkin <itkin.m@gmail.com>
2017-01-23 23:23:14 +0100

    Add WIP implementation of the export feature
    Create a Matchup object and give it an 'export' method
    NB: it's a dummy

---
+++ features/behave_export_matchup.feature
@@ -0,0 +1,14 @@
+Feature: Export resulting matchups
+
+  After collocation is complete the resulting object should contain data
+  It should be possible to save the data by simply calling a "export" method
+
+  The output format should either be guessed from the file extension or
+  "format" string should be provided
+
+  @wip
+  Scenario: Export netcdf matchups
+    Given a matchup object is available
+    When export command is called
+    And the output format is "netcdf"
+    Then a netcdf file should be saved on the disk
---
+++ features/steps/steps_export_matchup.py
@@ -0,0 +1,21 @@
+import os
+from trollmatch import match 
+
+@given(u'a matchup object is available')
+def step_impl(context):
+    context.matchup = match.Matchup()
+
+@when(u'the output format is "netcdf"')
+def step_impl(context):
+    context.matchup.output_format = "netcdf"
+
+@when(u'export command is called')
+def step_impl(context):
+    context.matchup.output_filepath = '/tmp/o.nc4'
+    context.matchup.export(context.matchup.output_filepath)
+
+
+@then(u'a netcdf file should be saved on the disk')
+def step_impl(context):
+    os.path.exists(context.matchup.output_filepath)
+    raise NotImplementedError(u'STEP: Then a netcdf file should be saved on the disk')
